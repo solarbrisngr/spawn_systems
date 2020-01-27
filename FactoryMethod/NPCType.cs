53ab5a51651dc93cf6cca8784ac9d4cd6c49df61
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-01-26 19:55:58 -0700

    Add files via upload

---
+++ FactoryMethod/NPCType.cs
@@ -0,0 +1,10 @@
+﻿namespace Pattern.FactoryMethod
+{
+    public enum NPCType
+    {
+        Farmer,
+        Beggar,
+        Shopowner,
+        Villager
+    }
+}
---
+++ FactoryMethod/Villager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Pattern.FactoryMethod
+{
+    public class Villager : INPC
+    {
+        public void Speak()
+        {
+            Debug.Log("Villager: I'm here!");
+        }
+    }
+}
